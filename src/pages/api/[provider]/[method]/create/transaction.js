05605d4ad8468482c783fa3f762365c6ab87e52d
vhratts <118541950+vratts@users.noreply.github.com>
2024-10-21 17:37:17 -0300

    Update api Routes and Utils Controller #3

---
+++ src/pages/api/[provider]/[method]/create/transaction.js
@@ -0,0 +1,21 @@
+import UtilsController from "../../../../../controllers/UtilsController";
+
+export default async (req, res) => {
+  try {
+    if (req.method === "POST") {
+      var provider = UtilsController.HelperProvider(req);
+      // Definindo o cabeçalho Cache-Control com stale-while-revalidate
+      res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=60");
+      res.status(200).json(await provider.createTransaction(req.body));
+    } else {
+      // Se não for POST, retorna erro 405 (Method Not Allowed)
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
